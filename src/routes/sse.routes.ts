bee4bed48b9b74d7e30fe0427a5ea7865b2e79b8
RCOM363 <rahullankeppanavar363@gmail.com>
2025-05-23 04:00:47 +0000

    fix: message format in sse

--- src/routes/sse.routes.ts
+++ src/routes/sse.routes.ts
@@ -1,31 +1,32 @@
-import { Router, Request,Response } from "express";
+import { Router, Request, Response } from "express";
 
 const router = Router();
 
 let clients: Response[] = [];
 
-router.route("/health-updates").get((req:Request, res:Response) => {
-    res.setHeader("Content-Type", "text/event-stream");
-    res.setHeader("Cache-control","no-cache");
-    res.setHeader('Connection', 'keep-alive');
+router.route("/health-updates").get((req: Request, res: Response) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.flushHeaders();
 
-    console.log("Client connected to SSE");
+  console.log("Client connected to SSE");
 
-    // Add client to the list
-    clients.push(res);
+  // Add client to the list
+  clients.push(res);
 
-     // On client disconnect, remove from list
-    req.on("close", () => {
+  // On client disconnect, remove from list
+  req.on("close", () => {
     console.log("Client disconnected from SSE");
-    clients = clients.filter(client => client !== res);
-    });
-
-})
+    clients = clients.filter((client) => client !== res);
+  });
+});
 
 export const sendHealthUpdates = (message: string) => {
-    for (const client of clients) {
-        client.write(`${message}\n`);
-    }
-}
+  for (const client of clients) {
+    client.write(`data: ${message}\n\n`);
+  }
+};
 
-export {router as sseRoutes};
+export { router as sseRoutes };
